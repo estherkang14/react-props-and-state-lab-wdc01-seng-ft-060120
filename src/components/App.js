4cc4dd10a78c0377551efe61ee67438434c8f0f8
estherkang14 <estherkang14@gmail.com>
2020-08-04 17:03:35 -0400

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import Pets from '../data/pets'
+
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
+
+const URL = '/api/pets'
+const QUERYURL = URL + '?type=' 
+
 
 class App extends React.Component {
   constructor() {
@@ -15,8 +21,46 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (e) => {
+    e.preventDefault()
+    
+    this.setState({
+      filters: {
+        type: e.target.value
+      }
+    }, () => console.log(this.state.filters))
+    
+  }
+
+  onFindPetsClick = (e) => {
+    e.preventDefault()
+    
+    if (this.state.filters.type === "all") {
+      fetch(URL)
+      .then(response => response.json())
+      .then(data => this.setState( {pets: data}))
+    } else {
+      fetch(QUERYURL + this.state.filters.type)
+      .then(response => response.json())
+      .then(data => this.setState( {pets: data}))
+    }
+
+  }
+
+  onAdoptPet = (e, petId) => {
+    
+      let petIndex = this.state.pets.findIndex(pet => pet.id === petId)
+
+      let newArray = [...this.state.pets]
+      newArray[petIndex] = {...newArray[petIndex], isAdopted: true}
+      this.setState({
+        pets: newArray
+      })
+   }
+
   render() {
     return (
+      
       <div className="ui container">
         <header>
           <h1 className="ui dividing header">React Animal Shelter</h1>
@@ -24,10 +68,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
---
+++ src/components/PetList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Pet from '../data/pets'
+
+function PetList(props) {
+    return( <div>
+        {console.log(props)}
+    </div>)
+}
+
+export default PetList
